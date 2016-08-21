edec8a7719314a3110a3a3893fa40d5472de0387
Elliott Slingsby <elliottslingsby@googlemail.com>
2016-08-21 21:56:13 +0100

    ObjectPoolTest

---
+++ test/ContainerTest.cpp
@@ -0,0 +1,80 @@
+#include <gtest\gtest.h>
+
+#include "TestComponent.hpp"
+
+#include "ObjectPool.hpp"
+#include <vector>
+#include <random>
+
+#define SIZE 4096
+#define OBJECTS 1024
+#define ITERATIONS 1024 * 64
+#define GETS 1024 * 64
+
+class ContainerTest : public ::testing::Test{
+protected:
+	BasePool* pool;
+	std::vector<BaseComponent*>* vector;
+
+	using TestComponent = TestComponent<SIZE>;
+
+	void SetUp() override{
+		pool = new ObjectPool<TestComponent>(TestComponent::chunk());
+		vector = new std::vector<BaseComponent*>;
+	
+		for (unsigned int i = 0; i < OBJECTS; i++){
+			pool->insert<TestComponent>(i);
+		}
+
+		for (unsigned int i = 0; i < OBJECTS; i++){
+			vector->insert(vector->end(), new TestComponent);
+		}
+
+		srand(time(0));
+	}
+
+	void TearDown() override{
+		delete pool;
+
+		for (auto component : *vector){
+			if (component)
+				delete component;
+		}
+
+		delete vector;
+	}
+};
+
+TEST_F(ContainerTest, Vector_Iteration){
+	for (unsigned int x = 0; x < ITERATIONS; x++){
+		for (unsigned int i = 0; i < OBJECTS; i++){
+			memset((void*)static_cast<TestComponent*>(vector->at(i))->memory, rand(), OBJECTS);
+		}
+	}
+}
+
+TEST_F(ContainerTest, ObjectPool_Iteration){
+	for (unsigned int x = 0; x < ITERATIONS; x++){
+		for (unsigned int i = 0; i < OBJECTS; i++){
+			memset((void*)pool->get<TestComponent>(i)->memory, rand(), OBJECTS);
+		}
+	}
+}
+
+TEST_F(ContainerTest, ObjectPool_Gets){
+	for (unsigned int x = 0; x < GETS; x++){
+		unsigned int i = rand() % OBJECTS;
+		memset((void*)pool->get<TestComponent>(i)->memory, rand(), OBJECTS);
+	}
+}
+
+TEST_F(ContainerTest, Vector_Gets){
+	for (unsigned int x = 0; x < GETS; x++){
+		unsigned int i = rand() % OBJECTS;
+		memset((void*)static_cast<TestComponent*>(vector->at(i))->memory, rand(), OBJECTS);
+	}
+}
+
+TEST(DEBUG, END){
+	getchar();
+}
--- test/TestHelper.cpp
+++
@@ -1,5 +0,0 @@
-#include "TestHelper.hpp"
-
-TestHelper::Timer::Timer(){
-	_start = _now();
-}
